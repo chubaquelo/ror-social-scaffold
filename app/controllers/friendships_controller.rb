9d96a885bdf5e3f8e1fe4d04226ba2c28e4d1e25
Sergio <sergioobolevich@gmail.com>
2020-12-01 19:57:00 -0300

    Change queries to modify 2 db rows for friendship.

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,7 +5,9 @@ class FriendshipsController < ApplicationController
 
   def create
     @friendship = current_user.friendships.build(params_friendship)
-    if @friendship.save
+    inv_user = User.find(params_friendship[:friend_id])
+    inv_fship = inv_user.friendships.build(friend_id: current_user.id)
+    if @friendship.save && inv_fship.save
       flash[:notice] = 'Friendship was saved correctly.'
       redirect_back(fallback_location: new_user_friendship_path)
     else
@@ -15,8 +17,9 @@ class FriendshipsController < ApplicationController
 
   def update
     user = User.find(params[:user_id])
-    if current_user.friend_requests.include?(user)
+    if current_user.friend_requests.include?(user) && user.friend_requests.include?(current_user)
       current_user.confirm_friend(user)
+      user.confirm_friend(current_user)
       flash[:notice] = 'Friendship was confirmed correctly.'
       redirect_back(fallback_location: user_path)
     else
