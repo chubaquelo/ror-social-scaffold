0a9ade302df0bb51e0e58dacf303afffb9eedb7d
Sergio <sergioobolevich@gmail.com>
2020-12-01 14:04:21 -0300

    Add rspec test for Post, comments models, and some features.

---
+++ spec/features/create_comment_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'comment creation', type: :feature do
+  let(:user) { User.create(name: 'Johnny Bravo', email: 'johnny@microverse.org', password: '123456789') }
+
+  scenario 'create comment' do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+    fill_in 'post_content', with: 'This is a new post for testing.'
+    click_on 'Save'
+    fill_in 'comment_content', with: 'This is a new comment for testing.'
+    click_on 'Comment'
+    expect(page).to have_content('Johnny Bravo: This is a new comment for testing.')
+  end
+
+end
---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'session create and delete', type: :feature do
+  let(:user) { User.create(name: 'Johnny Bravo', email: 'johnny@microverse.org', password: '123456789') }
+  
+  scenario 'login' do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+    expect(page).to have_content("Signed in successfully.")
+    visit user_path(user.id)
+    expect(page).to have_content('Johnny Bravo')
+  end
+
+  scenario 'logout' do
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_on 'Log in'
+    click_on 'Sign out'
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+  end
+end
