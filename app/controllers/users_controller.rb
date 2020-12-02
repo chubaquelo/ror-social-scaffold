9b4bad4d7943bf70af06af01a443b4447f74e027
Sergio <sergioobolevich@gmail.com>
2020-12-02 13:03:46 -0300

    Solve send duplicated send friend request. Add Profile to navbar. Add requests list to profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @friend_requests = current_user.friend_requests
     @user = User.find(params[:id])
     @posts = @user.posts.ordered_by_most_recent
   end
