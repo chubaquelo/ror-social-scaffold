fd2610e57091203098575fe7d08557df50d45792
ricardo <ramn21@gmail.com>
2020-12-03 17:12:37 +0100

    add pending associations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,15 +15,17 @@ class User < ApplicationRecord
   has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
   has_many :friends, through: :confirmed_friendships
 
+  has_many :pending_friendships, -> { where confirmed: nil }, class_name: "Friendship", foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
   # def friends
   #   friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
   #   friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
   #   friends_array.compact
   # end
 
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
+  # def pending_friends
+  #   friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
+  # end
 
   def friend_requests
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
